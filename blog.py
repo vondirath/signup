8e6696f90218f52515302aed30c0fbe7fd360d88
Thomas Lopez <Vondirath@gmail.com>
2016-08-24 16:15:24 -0600

    First

---
+++ blog.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import jinja2
+import webapp2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                              autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+        
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+def render_post(response, post):
+    response.out.write('<b>' + post.subject + '</b><br>')
+    response.out.write(post.content) 
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+        
+class Blog(Handler):
+    def get(self):
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('blog.html', posts = posts)
+
+class PostPage(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
+
+class NewPost(Handler):
+    def get(self):
+        self.render('newpost.html')
+
+    def post(self):
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+        
+        if subject and content:
+            a = Post(parent = blog_key(), subject = subject, content = content)
+            a.put()
+            self.redirect('/blog/%s' % str(a.key().id()))
+        else:
+            error = "You are missing some info"
+            self.render("newpost.html", subject = subject, content = content, error = error)
+
+app = webapp2.WSGIApplication([("/blog/?", Blog), ('/blog/newpost', NewPost),('/blog/([0-9]+)', PostPage)], debug=True)
